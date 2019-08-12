e68065593003ab2354ce31af065349b2e9a89ce9
Balazs Lendvay <lendvayb@gmail.com>
2019-08-13 00:30:23 +0200

    Feature update, credential store

--- bCIRT/testing/parse_csv.py
+++ bCIRT/testing/parse_csv.py
@@ -1,16 +1,4 @@
 #!/usr/env/python
-# -*- coding: utf-8 -*-
-# **********************************************************************;
-# Project           : bCIRT
-# License           : GPL-3.0
-# Program name      : accounts/admin.py
-# Author            : Balazs Lendvay
-# Date created      : 2019.07.27
-# Purpose           : Admin file for the bCIRT
-# Revision History  : v1
-# Date        Author      Ref    Description
-# 2019.07.29  Lendvay     1      Initial file
-# **********************************************************************;
 import argparse
 import csv
 
